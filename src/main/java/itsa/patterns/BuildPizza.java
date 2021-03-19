3827ad4c27452b8b448edfc5d0714dd202740ae3
Jia Cheng <cif-jiacheng.teo@ninjavan.co>
2021-03-20 04:57:28 +0800

    Update BuildPizza.java

--- src/main/java/itsa/patterns/BuildPizza.java
+++ src/main/java/itsa/patterns/BuildPizza.java
@@ -4,16 +4,16 @@ package itsa.patterns;
 class Pizza {
     private String dough = "";
     private String sauce = "";
-	private String topping = "";
-	
-	public void setDough(String dough) {
+    private String topping = "";
+
+    public void setDough(String dough) {
         this.dough = dough;
     }
 
     public String getDough() {
         return dough;
     }
-	
+
     public void setSauce(String sauce) {
         this.sauce = sauce;
     }
@@ -21,11 +21,12 @@ class Pizza {
     public String getSauce() {
         return sauce;
     }
+
     public void setTopping(String topping) {
         this.topping = topping;
     }
-	
-	public String getTopping() {
+
+    public String getTopping() {
         return topping;
     }
 }
@@ -34,27 +35,62 @@ class Pizza {
 abstract class PizzaBuilder {
     protected Pizza pizza;
 
-    public String getPizza() {
-        return pizza.getDough()+pizza.getSauce()+pizza.getTopping();
-    }
     public void createNewPizzaProduct() {
         pizza = new Pizza();
     }
-	// you need to add the abstract methods for the steps of building a pizza - buildDough, buildSauce and buildTopping.
+
+    public String getPizza() {
+        return pizza.getDough() + pizza.getSauce() + pizza.getTopping();
+    }
+
+    // you need to add the abstract methods for the steps of building a pizza - buildDough, buildSauce and buildTopping.
+    public abstract void buildDough();
+
+    public abstract void buildSauce();
+
+    public abstract void buildTopping();
 
 }
 
 /* "ConcreteBuilder" */
 class HawaiianPizzaBuilder extends PizzaBuilder {
+
     // you need to implement the abstract methods of the PizzaBuilder
     // the dough is "cross", sauce is "mild", topping is "pineapple"
+    @Override
+    public void buildDough() {
+        pizza.setDough("cross");
+    }
+
+    @Override
+    public void buildSauce() {
+        pizza.setSauce("mild");
+    }
+
+    @Override
+    public void buildTopping() {
+        pizza.setTopping("pineapple");
+    }
 }
 
 /* "ConcreteBuilder" */
 class SpicyPizzaBuilder extends PizzaBuilder {
-	// you need to implement the abstract methods of the PizzaBuilder
-   // the dough is "baked", sauce is "hot", topping is "pepperoni"
+    // you need to implement the abstract methods of the PizzaBuilder
+    // the dough is "baked", sauce is "hot", topping is "pepperoni"
+    @Override
+    public void buildDough() {
+        pizza.setDough("baked");
+    }
 
+    @Override
+    public void buildSauce() {
+        pizza.setSauce("hot");
+    }
+
+    @Override
+    public void buildTopping() {
+        pizza.setTopping("pepperoni");
+    }
 }
 
 /* "Director" */
@@ -70,30 +106,33 @@ class Waiter {
     }
 
     public void constructPizza() {
-		// you need to put the generic construction of the pizza here.
+        // you need to put the generic construction of the pizza here.
         pizzaBuilder.createNewPizzaProduct();
+        pizzaBuilder.buildDough();
+        pizzaBuilder.buildSauce();
+        pizzaBuilder.buildTopping();
     }
 }
 
 /* A customer ordering a pizza. */
 public class BuildPizza {
     public static void main(String[] args) {
-		
+
         Waiter waiter = new Waiter();
         PizzaBuilder hawaiianPizzabuilder = new HawaiianPizzaBuilder();
         PizzaBuilder spicyPizzaBuilder = new SpicyPizzaBuilder();
 
-        waiter.setPizzaBuilder( hawaiianPizzabuilder );
+        waiter.setPizzaBuilder(hawaiianPizzabuilder);
         waiter.constructPizza();
         String pizza = waiter.getPizza();
-		System.out.println(pizza);
-		assert pizza.equals("crossmildpineapple") : "Pizza is not right";
-		
-		waiter.setPizzaBuilder( spicyPizzaBuilder );
+        System.out.println(pizza);
+        assert pizza.equals("crossmildpineapple") : "Pizza is not right";
+
+        waiter.setPizzaBuilder(spicyPizzaBuilder);
         waiter.constructPizza();
         pizza = waiter.getPizza();
-		System.out.println(pizza);
-		assert pizza.equals("bakedhotpepperoni") : "Pizza is not right";
+        System.out.println(pizza);
+        assert pizza.equals("bakedhotpepperoni") : "Pizza is not right";
 
     }
 }
