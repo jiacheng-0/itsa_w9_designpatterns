34323f866f00994dbb3ec954e62d1bb436b5184f
Jia Cheng <cif-jiacheng.teo@ninjavan.co>
2021-03-21 04:26:55 +0800

    Add: make use of adapter attribute in each of Shape's Subclasses

--- src/main/java/itsa/patterns/AdapterExample.java
+++ src/main/java/itsa/patterns/AdapterExample.java
@@ -22,38 +22,37 @@ interface Shape {
 }
 
 class LineAdapter implements Shape {
+
     //Skeleton constructors
     // public LineAdapter() {}
-
-    private Line adapter;
+    private final Line adapter;
 
     public LineAdapter(Line line) {
         this.adapter = line;
     }
 
+    @Override
     public String draw(int x1, int y1, int x2, int y2) {
         // add your codes
-        return "";
-        // Line line = new line(x1, y1, x2, y2);
-        // return line;
+        return adapter.draw(x1, y1, x2, y2);
     }
 }
 
 class RectangleAdapter implements Shape {
     private Rectangle adapter;
 
-    // public RectangleAdapter() {        }
     public RectangleAdapter(Rectangle rectangle) {
         this.adapter = rectangle;
     }
 
+    @Override
     public String draw(int x1, int y1, int x2, int y2) {
         // You can use the following conversion codes
         int x = Math.min(x1, x2);
         int y = Math.min(y1, y2);
         int width = Math.abs(x2 - x1);
         int height = Math.abs(y2 - y1);
-        return "";
+        return adapter.draw(x, y, width, height);
     }
 }
 
@@ -81,7 +80,7 @@ public class AdapterExample {
             } else if (shape.getClass().getSimpleName().equals("Rectangle")) {
                 message = ((Rectangle) shape).draw(x1, y1, width, height);
             }
-            System.out.println(message);
+            // System.out.println(message);
             assert message.equals("Line from point A(1;1), to point B(3;6)") || message.equals("Rectangle with starting left lowest point (1;1), width: 2, height: 5") : "Message does not matched";
         }
     }
