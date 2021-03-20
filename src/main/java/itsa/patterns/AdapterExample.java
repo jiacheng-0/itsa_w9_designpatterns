76c1e33618260518db2550aa3b4058b861ec2dbc
Jia Cheng <cif-jiacheng.teo@ninjavan.co>
2021-03-21 04:17:58 +0800

    Format Adapter

--- src/main/java/itsa/patterns/AdapterExample.java
+++ src/main/java/itsa/patterns/AdapterExample.java
@@ -1,9 +1,8 @@
-
 package itsa.patterns;
 
 // adapter
 class Line {
-		  
+
     public String draw(int x1, int y1, int x2, int y2) {
         return "Line from point A(" + x1 + ";" + y1 + "), to point B(" + x2 + ";" + y2 + ")";
     }
@@ -11,10 +10,50 @@ class Line {
 
 // adapter
 class Rectangle {
-	
+
     public String draw(int x, int y, int width, int height) {
-        return "Rectangle with starting left lowest point (" + x + ";" + y + "), width: " + width
-                + ", height: " + height;
+        return "Rectangle with starting left lowest point (" + x + ";" + y + "), width: " + width + ", height: " + height;
+    }
+}
+
+// Use this classes, interface and method skeleton. You still need to provide the methods in LineAdapter and RectangleAdapter
+interface Shape {
+    String draw(int x, int y, int z, int j);
+}
+
+class LineAdapter implements Shape {
+    //Skeleton constructors
+    // public LineAdapter() {}
+
+    private Line adapter;
+
+    public LineAdapter(Line line) {
+        this.adapter = line;
+    }
+
+    public String draw(int x1, int y1, int x2, int y2) {
+        // add your codes
+        return "";
+        // Line line = new line(x1, y1, x2, y2);
+        // return line;
+    }
+}
+
+class RectangleAdapter implements Shape {
+    private Rectangle adapter;
+
+    // public RectangleAdapter() {        }
+    public RectangleAdapter(Rectangle rectangle) {
+        this.adapter = rectangle;
+    }
+
+    public String draw(int x1, int y1, int x2, int y2) {
+        // You can use the following conversion codes
+        int x = Math.min(x1, x2);
+        int y = Math.min(y1, y2);
+        int width = Math.abs(x2 - x1);
+        int height = Math.abs(y2 - y1);
+        return "";
     }
 }
 
@@ -28,60 +67,22 @@ public class AdapterExample {
         // int x2 = 3, y2 = 6;
         // for (Shape shape : shapes) {
         //  shape.draw(x1, y1, x2, y2);
-        // } 
+        // }
 
         Object[] shapes = {new Line(), new Rectangle()};
         int x1 = 1, y1 = 1;
         int x2 = 3, y2 = 6;
         int width = 2, height = 5;
-        
-        for (Object shape : shapes) {
-			String message = "";
+
+        for (Object shape: shapes) {
+            String message = "";
             if (shape.getClass().getSimpleName().equals("Line")) {
-                message = ((Line)shape).draw(x1, y1, x2, y2);
+                message = ((Line) shape).draw(x1, y1, x2, y2);
             } else if (shape.getClass().getSimpleName().equals("Rectangle")) {
-                message = ((Rectangle)shape).draw(x1, y1, width, height);
+                message = ((Rectangle) shape).draw(x1, y1, width, height);
             }
-			System.out.println(message);	
-			assert message.equals("Line from point A(1;1), to point B(3;6)") || message.equals("Rectangle with starting left lowest point (1;1), width: 2, height: 5") : "Message does not matched";
+            System.out.println(message);
+            assert message.equals("Line from point A(1;1), to point B(3;6)") || message.equals("Rectangle with starting left lowest point (1;1), width: 2, height: 5") : "Message does not matched";
         }
     }
 }
-
-// Use this classes, interface and method skeleton. You still need to provide the methods in LineAdapter and RectangleAdapter
-interface Shape {
-    String draw(int x, int y, int z, int j);
-}
-class LineAdapter implements Shape {
-	//Skeleton constructors
-	// public LineAdapter() {}
-    
-    private Line adapter;
-    public LineAdapter(Line line) {
-        this.adapter = line;
-    }
-	
-	public String draw(int x1, int y1, int x2, int y2) {
-        // add your codes
-        return "";
-        // Line line = new line(x1, y1, x2, y2);
-		// return line;
-    }
-}
-class RectangleAdapter implements Shape {
-    private Rectangle adapter;
-	// public RectangleAdapter() {        }
-	public RectangleAdapter(Rectangle rectangle) {
-        this.adapter = rectangle;
-    }
-
-	public String draw(int x1, int y1, int x2, int y2) {
-        // You can use the following conversion codes
-		int x = Math.min(x1, x2);
-        int y = Math.min(y1, y2);
-        int width = Math.abs(x2 - x1);
-        int height = Math.abs(y2 - y1);
-		return "";
-    }
-}
-
