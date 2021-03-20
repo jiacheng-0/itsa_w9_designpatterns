c21a251d5006f6a7ea82cff6bb38a986c336fde9
Jia Cheng <cif-jiacheng.teo@ninjavan.co>
2021-03-21 04:15:50 +0800

    Add Facade: ShapeMaker Class

--- src/test/java/itsa/patterns/FacadeExampleTest.java
+++ src/test/java/itsa/patterns/FacadeExampleTest.java
@@ -11,7 +11,6 @@ public class FacadeExampleTest {
       ShapeMaker shapeMaker = new ShapeMaker();
  
       String message = shapeMaker.draw();
-	  assert message.equals("Circle::draw()Rectangle::draw()Square::draw()") : "Message does not matched";
-
+	  assert message.equals("Circle::draw()Rectangle::draw()Square::draw()") : "Message does not match";
 	}
 }
